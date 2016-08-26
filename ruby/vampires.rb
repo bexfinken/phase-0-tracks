507cf4b63f96cce588104b2939670bb9e266849d
bexfinken <bex.finken@gmail.com>
2016-08-25 22:57:05 -0500

    write program to collect vampire info

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What's your name?"
+	name = gets.chomp
+
+puts "How old are you? What year were you born?"
+	age = gets.chomp.to_i 
+	year = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp
