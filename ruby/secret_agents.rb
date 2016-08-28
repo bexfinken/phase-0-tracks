6e8228f51c4d14b8ddb12d6f9b4fd9e72c5f9492
bexfinken <bex.finken@gmail.com>
2016-08-28 08:51:54 -0500

    create encrypt code

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,17 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+password = "bcd"
+index = 0
+
+while index < alphabet.length
+
+	pw = password[0]
+	az = alphabet[index]
+
+	if pw == az 
+		puts "yay"
+	else
+		puts "wah" 
+	end 	
+	
+	index += 1
+end 
