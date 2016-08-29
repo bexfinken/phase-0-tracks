571a1fbe62caace24bee04a6148fd535e1c2a494
bexfinken <bex.finken@gmail.com>
2016-08-29 08:52:44 -0500

    create code for decrypt program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,3 +7,31 @@ password = "abc"
 	end
 end
 
+encrypt("")
+
+
+def decrypt(password)
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+counter = 0
+
+
+until counter == password.length
+	pw = password[counter]
+	index = 0	
+	while index < alphabet.length
+		
+		az = alphabet[index]
+
+		if pw == az
+			print alphabet[index-1]
+		end 	
+	
+		index += 1
+	end 
+	counter += 1
+end
+
+end 
+
+decrypt("password")
