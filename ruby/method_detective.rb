2785815e4a10373877f34cb80fddfb9ae5c3af75
bexfinken <bex.finken@gmail.com>
2016-08-31 22:00:05 -0500

    find the methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,11 +1,12 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
+Replace in the "<???>" with the appropriate method (and arguments, if any).
+Uncomment the calls to catch these methods red-handed.
 
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
+ # or alt_case
 
 # "zom".<???>
 # => “zoom”
@@ -13,27 +14,34 @@
 # "enhance".<???>
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase!
+=> "STOP! YOU’RE UNDER ARREST!"
+#It would be upcase if there was a new string, but there's not.
 
-# "the usual".<???>
+
+a = "the usual"
+a << " suspects"
 #=> "the usual suspects"
 
 # " suspects".<???>
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+s = "The mystery of the missing first letter"
+s = s[1..-1]
+=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+
+"Elementary,    my   dear        Watson!".lstrip
+=> "Elementary, my dear Watson!"
 
 # "z".<???>
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+(What is the significance of the number 122 in relation to the character z?)
+The ascii code for z is 122 
+
 
 # "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+# => 
