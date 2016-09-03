f0eaeaaadff5e71c505e482b15560b42c5c75429
bexfinken <bex.finken@gmail.com>
2016-09-03 14:27:18 -0500

    Complete 5.1 Release 1

--- ruby/phase-0-tracks/ruby/method_detective.rb
+++ ruby/phase-0-tracks/ruby/method_detective.rb
@@ -1,39 +1,54 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
+Replace in the "<???>" with the appropriate method (and arguments, if any).
+Uncomment the calls to catch these methods red-handed.
 
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"zom".insert(1, 'o') 
+=> “zoom”
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+puts "\tenhance\t"
+=> "    enhance    "
 
-# " suspects".<???>
-# => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"Stop! You’re under arrest!".upcase!
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+a = "the usual "
+a << "suspects"
+=> "the usual suspects"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+a = "the usual "
+" suspects".insert(1, a) 
+=> "the usual suspects"
+
+
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
+
+s = "The mystery of the missing first letter"
+s = s[1..-1]
+=> "he mystery of the missing first letter"
+
+
+"Elementary,    my   dear        Watson!".lstrip
+=> "Elementary, my dear Watson!"
+
+
+"z".each_byte do |c|
+    puts c
+end
+=> 122 
+(What is the significance of the number 122 in relation to the character z?)
+The ascii code for z is 122 
+
+
+"How many times does the letter 'a' appear in this string?".count "a"
+=> 4  
