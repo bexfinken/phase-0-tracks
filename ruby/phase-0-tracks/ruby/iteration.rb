cebd0f5e9bb40fecdd966605989d8c2c9b06f73d
bexfinken <bex.finken@gmail.com>
2016-09-03 14:54:23 -0500

    delete duplicate files

--- ruby/phase-0-tracks/ruby/iteration.rb
+++
@@ -1,9 +0,0 @@
-def pet_names
-	pet1 = "Hugo"
-	pet2 = "Princess Unicorn"
-	pet3 = "Gilbert Houndsworth"
-	puts "What are your pets' names?"
-	yield(pet1, pet2, pet3)
-end
-
-pet_names { |pet1, pet2, pet3| puts "My pets' names are #{pet1}, #{pet2}, and #{pet3}." }
