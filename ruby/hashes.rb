723e8c343324f468148a7269126f38c4efe9954e
bexfinken <bex.finken@gmail.com>
2016-09-03 16:20:15 -0500

    Initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# Initialize and manipulate arrays
+# Initialize and manipulate hashes
+# Use variables to access arrays
+# Use variables to access hashes
+# Experiment with expressions that reference data structures
+# Integrate a data structure into a simple program
+
+
