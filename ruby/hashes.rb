eba32a9f1c6aa66741ce2c822c7bba2c8409e858
bexfinken <bex.finken@gmail.com>
2016-09-04 06:51:02 -0500

    Change text

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,7 +20,7 @@ puts "What is your name?"
 client_name = gets.chomp
 
 puts "Age, please?"
-client_age = gets.chomp
+client_age = gets.chomp.to_i
 
 puts "Your address?"
 client_address = gets.chomp
@@ -31,45 +31,72 @@ client_email = gets.chomp
 puts "Your phone number?"
 phone_number = gets.chomp
 
-puts "How do you prefer to be contacted?"
+puts "Do you prefer to be contacated by phone or email?"
 preferred_contact = gets.chomp
 
-puts "Do you have children?"
+puts "Do you have children? (yes or no)"
 kids = gets.chomp
 	if kids == "yes"
-		puts "Are you interested in wear-resistant hardwood floors?"
-		hardwood_floors = gets.chomp
-			if hardwood_floors == yes
-				puts "Great! We have a great selection for you."
-			else 
-				puts "Great!"
-			end 
-	else
-		puts "Okay!"
+		kids = true
+	elsif kids == "no"
+		kids = false 
+	else puts "Sorry, I didn't catch that. Please say yes or no."
+		if kids == "yes"
+			kids = true
+		else kids == "no"
+			kids = false 
+		end 
 	end
 
-puts "Do you have pets?"
+puts "Do you have pets? (yes or no)"
 pets = gets.chomp
 	if pets == "yes"
-		puts "Are you interested in wear-resistant hardwood floors?"
-		hardwood_floors = gets.chomp
-			if hardwood_floors == yes
-				puts "Great! We have a great selection for you."
-			else 
-				puts "Great!"
-			end 
-	else
-		puts "Okay!"
+		pets = true
+	elsif pets == "no"
+		pets = false 
+	else puts "Sorry, I didn't catch that. Please say yes or no."
+		if pets == "yes"
+			pets = true
+		else pets == "no"
+			pets = false 
+		end 
 	end
+
+if pets || kids
+	puts "Are you interested in wear-resistant hardwood floors?"
+	hardwood_floors = gets.chomp
+	if hardwood_floors == "yes"
+		puts "Great! We have a great selection for you."
+		hardwood_floors = true 
+	elsif 
+		hardwood_floors == "no"
+		puts "I'm sure we have other great choices for you"
+		hardwood_floors = false 
+	else
+		puts "Sorry, I didn't catch that. Please say yes or no."
+		hardwood_floors = gets.chomp
+		if hardwood_floors == "yes"
+			puts "Great! We have a great selection for you."
+			hardwood_floors = true 
+		else 
+			hardwood_floors == "no"
+			puts "I'm sure we have other great choices for you"
+			hardwood_floors = false 
+		end	
+	end
+end 
 	
 puts "What is your theme?"
-theme = gets.chomp
+client_theme = gets.chomp
+puts "That sounds lovely!"
 
 puts "What's your favorite color?"
 color = gets.chomp
+puts "Great color!"
 
 puts "What's your budget?"
-max_budget = gets.chomp
+max_budget = gets.chomp.to_i
+puts "We will makeyour home beautiful!"
 
 form = {}
 form["name"] = client_name
