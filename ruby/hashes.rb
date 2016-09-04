7da6ef21cb119465b08fc6842a2c6ecd361d92f6
bexfinken <bex.finken@gmail.com>
2016-09-04 08:55:06 -0500

    Complete 5.2 Release 3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,4 @@
+#get client info:
 #name
 #age
 #address
@@ -5,36 +6,35 @@
 #phone
 #preferred way to contact
 #kids y/n
-#  how many?
 #pets y/n
-# if yes, hardwood or scotchguard carpet?
+# if yes, flooring suggestion?
 #theme
 #favorite color
 #budget 
+#new form information
+#prompt for info change
+#gather new info
+#print form with updated client information
 
-
-
-#DON"T FORGET TO CHANGE THE PET QUESTION
-
-puts "What is your name?"
+puts "What is the client's name?"
 client_name = gets.chomp
 
-puts "Age, please?"
+puts "Client's age?"
 client_age = gets.chomp.to_i
 
-puts "Your address?"
+puts "Address?"
 client_address = gets.chomp
 
-puts "Your email?"
+puts "Email?"
 client_email = gets.chomp
 
-puts "Your phone number?"
+puts "Phone number?"
 phone_number = gets.chomp
 
-puts "Do you prefer to be contacated by phone or email?"
+puts "Do they prefer to be contacated by phone or email?"
 preferred_contact = gets.chomp
 
-puts "Do you have children? (yes or no)"
+puts "Do they have children? (yes or no)"
 kids = gets.chomp
 	if kids == "yes"
 		kids = true
@@ -48,7 +48,7 @@ kids = gets.chomp
 		end 
 	end
 
-puts "Do you have pets? (yes or no)"
+puts "Do they have pets? (yes or no)"
 pets = gets.chomp
 	if pets == "yes"
 		pets = true
@@ -63,53 +63,75 @@ pets = gets.chomp
 	end
 
 if pets || kids
-	puts "Are you interested in wear-resistant hardwood floors?"
+	puts "Ask if they're interested in wear-resistant hardwood floors"
 	hardwood_floors = gets.chomp
 	if hardwood_floors == "yes"
-		puts "Great! We have a great selection for you."
+		puts "Great! We have a great selection."
 		hardwood_floors = true 
 	elsif 
 		hardwood_floors == "no"
-		puts "I'm sure we have other great choices for you"
+		puts "I'm sure we have lots of great choices"
 		hardwood_floors = false 
 	else
 		puts "Sorry, I didn't catch that. Please say yes or no."
 		hardwood_floors = gets.chomp
 		if hardwood_floors == "yes"
-			puts "Great! We have a great selection for you."
+			puts "Great! We have a great selection."
 			hardwood_floors = true 
 		else 
 			hardwood_floors == "no"
-			puts "I'm sure we have other great choices for you"
+			puts "I'm sure we have lots of great choices"
 			hardwood_floors = false 
 		end	
 	end
 end 
 	
-puts "What is your theme?"
+puts "Client's theme?"
 client_theme = gets.chomp
 puts "That sounds lovely!"
 
-puts "What's your favorite color?"
-color = gets.chomp
-puts "Great color!"
+puts "Client's favorite color?"
+favorite_color = gets.chomp
+puts "#{favorite_color} sounds pretty!"
 
-puts "What's your budget?"
+puts "What's the client's budget?"
 max_budget = gets.chomp.to_i
-puts "We will makeyour home beautiful!"
+puts "We can certainly work with that!"
 
 form = {}
-form["name"] = client_name
-form["age"] = client_age
-form["address"] = client_address
-form["email"] = client_email
-form["phone"] = phone_number
-form["contact"] = preferred_contact
-form["children"] = kids
-form["pets"] = pets
-form["floor"] = hardwood_floors
-form["theme"] = client_theme
-form["color"] = favorite_color
-form["budget"] = max_budget
+form[:name] = client_name
+form[:age] = client_age
+form[:address] = client_address
+form[:email] = client_email
+form[:phone] = phone_number
+form[:contact] = preferred_contact
+form[:children] = kids
+form[:pets] = pets
+form[:floor] = hardwood_floors
+form[:theme] = client_theme
+form[:color] = favorite_color
+form[:budget] = max_budget
 
 p form 
+puts "Would you like to update any info?"
+update = gets.chomp
+if update == "yes"
+	puts "Please enter the category you'd like to change"
+	key = gets.to_sym
+	puts "Enter the new information"
+	input = gets.chomp
+	form [key] = [input]
+else 
+	puts "Great--see you next time!"
+end
+
+p form 
+
+
+
+
+
+
+
+
+
