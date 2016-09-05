abfaab0ecc0843b3a3cc58609bb2ec6d66c934b5
bexfinken <bex.finken@gmail.com>
2016-09-05 00:54:28 -0500

    Initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+skeleton = {
+    axial_skeleton: {
+        vertebral_column: {
+            cervical_vertebra: 7,
+            thoracic_vertebra: 12,
+            lumbar_vertebra: 5.
+        }
+            
+        thoracic_cage: {
+            fixed_ribs: 7,
+            floating_ribs: 3,
+            false_ribs: 2
+        }
+        
+        skull: [
+            "neurocranium"
+            "viscerocranium"
+        ]
+    },
+    appendicular_skeleton: {
+        upper_limbs: [
+        	"femur"
+        	"tibia"
+        	"patella"
+        	]
+        	
+        lower_limbs: [
+					"ulna"
+					"radius"
+					"humerus"
+        ]
+    }
+}
+
+
