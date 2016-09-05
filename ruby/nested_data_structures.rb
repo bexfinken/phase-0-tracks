936f58100a7c8cec8c838e63d15dc5cb60f41462
bexfinken <bex.finken@gmail.com>
2016-09-05 01:09:57 -0500

    Clean up code

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,31 +3,31 @@ skeleton = {
         vertebral_column: {
             cervical_vertebra: 7,
             thoracic_vertebra: 12,
-            lumbar_vertebra: 5.
-        }
+            lumbar_vertebra: 5
+        },
             
         thoracic_cage: {
             fixed_ribs: 7,
             floating_ribs: 3,
             false_ribs: 2
-        }
+        },
         
         skull: [
-            "neurocranium"
+            "neurocranium",
             "viscerocranium"
         ]
     },
     appendicular_skeleton: {
         upper_limbs: [
-        	"femur"
-        	"tibia"
-        	"patella"
-        	]
-        	
+            "femur",
+            "tibia",
+            "patella"
+            ],
+            
         lower_limbs: [
-					"ulna"
-					"radius"
-					"humerus"
+            "ulna",
+            "radius",
+            "humerus"
         ]
     }
 }
