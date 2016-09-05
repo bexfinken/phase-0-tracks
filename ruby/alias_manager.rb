361d5f70466ea8d677e0b30cad9374d9b4b593d3
bexfinken <bex.finken@gmail.com>
2016-09-05 06:52:48 -0500

    Pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+# Felicia Torres
+# Swap first and last names .reverse
+# Define the vowels
+# Get the vowels to change to the .next vowel
+# Define consonants
+# Get the consonants to change to the .next consonant
+
