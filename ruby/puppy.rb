27e501179f4705d51cc2ab264085a81fd86d09af
bexfinken <bex.finken@gmail.com>
2016-09-07 11:31:02 -0500

    Puppy class behavior & attributes

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES
+# Annoyingus Puppylus
+
+# CHARACTERISTICS
+# Legs: 4 
+# Ears: floppy
+# Tail: varies
+# Name: varies
+# Fur color: varies
+
+# BEHAVIOR
+# Barks
+# Licks
+# Chews
+# Destroys
+
