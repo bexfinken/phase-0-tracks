c4c2fe95b82bd76180a9de1fa8d881dcccb8c163
bexfinken <bex.finken@gmail.com>
2016-09-09 09:09:11 -0500

    Complete 6.2 Release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,27 +1,76 @@
-class Puppy
+# # 6.2 Mandatory Pairing: Instance Methods
+
+# Release 1
+# class Puppy
+# end
+
+# duchess = Puppy.new
+# fido = Puppy.new
+# spot = Puppy.new
+# gilbert = Puppy.new	
+
+# class Puppy
+
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#  	end
+
+# 	def bark(times)
+#    puts "BARK!" * times
+# 	end
+
+# 	def speak(times)
+# 		puts "Woof! " * times
+# 	end
+
+# 	def roll_over
+# 		puts "**rolls over**"
+# 	end
+
+# 	def dog_years(human_years)
+# 		puts  "#{human_years.to_i}  equal to #{human_years.to_i * 7} in dog years"
+# 	end
+
+# 	def play_dead
+# 		puts "**plays dead**"
+# 	end 	
+
+# 	def initialize
+# 		puts "Initializing new puppy instance ..."
+# 	end
+
+# end
+
+
+# fido = Puppy.new
+
+
+# Release 2
+
+class Flower
 end
 
-duchess = Puppy.new
-fido = Puppy.new
-spot = Puppy.new
+rose = Flower.new
+daisy = Flower.new
+tulip = Flower.new
 
 
-class Puppy
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-end
+class Flower 
 
-class Puppy
-	def bark(times)
-   puts "BARK!" * times
- end
-end
-
-class Puppy	
-	def initialize
+	def pluck
+		puts "Hey, that's mine!"
 	end 
-end
 
-spot.initialize
+	def initialize
+		puts "Initializing new flower instance..."
+	end 
+
+	def flower.each do |flower|
+		puts "#{flower}"
+		50.times do |flower|
+	 
+end 
+
+
+initialize("tulip")
