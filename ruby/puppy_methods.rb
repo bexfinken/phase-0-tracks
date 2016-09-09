0787911ab931b56804a2c597c971e00bd5ec06ea
bexfinken <bex.finken@gmail.com>
2016-09-09 05:57:28 -0500

    Add puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,27 @@
 class Puppy
+end
 
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+
+class Puppy
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
 end
 
+class Puppy
+	def bark(times)
+   puts "BARK!" * times
+ end
+end
+
+class Puppy	
+	def initialize
+	end 
+end
+
+spot.initialize
