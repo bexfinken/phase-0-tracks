4301289aa191fe0c5eade43ca748e0dac12c54dc
bexfinken <bex.finken@gmail.com>
2016-09-09 13:39:38 -0500

    Complete 6.2 Release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,94 @@
+# # 6.2 Mandatory Pairing: Instance Methods
+# Release 1
+
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+gilbert = Puppy.new	
+
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+# def fetch(toy)
+# 	puts "I brought back the #{toy}!"
+# end
 
-end
+# def bark(times)
+# 	puts "BARK!" * times.to_i
+# end
+
+# def speak(times)
+# 	puts "Woof! " * times
+# end
+
+# def roll_over
+# 	puts "**rolls over**"
+# end
+
+# def dog_years(human_years)
+# 	puts  "#{human_years.to_i} human years is equal to #{human_years.to_i * 7} dog years"
+# end
+
+# def play_dead
+# 	puts "**plays dead**"
+# end 	
+
+# 	def initialize
+# 		puts "Initializing new puppy instance ..."
+# 	end
+
+# end
+
+
+
+
+
+
+
+
+
+# # Release 2
+# Make a loop to make 50 instances of whatever class.
+# The loop has to store all the instances in a data structure [array]
+# Iterate over that data structure using .each
+# call the instance methods you wrote on each instance. 
+# So if you wrote a Gymnast class, this is where you'd call .flip and .jump on each of your instances of Gymnast.
+
+
+# rose = Flower.new
+# daisy = Flower.new
+# tulip = Flower.new
+
+
+# class Flower 
+
+# 	def pluck(petal)
+# 		arr = []
+# 		flower << arr
+# 		puts "ouch!"
+# 	end
+
+# 	def initialize
+# 		puts "Initializing new flower instance..."
+# 	end 
+
+# end 
+
+
+# daisy = Flower.new
+
+
+
+
+# # 	def flower.each do |flower|
+# # 		puts "#{flower}"
+# # 		50.times do |flower|
+	 
+# # end 
+
+
+# # initialize("tulip")
 
