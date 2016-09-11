30e787d7b00ef7e4f8460d64da9f1c3985241df6
bexfinken <bex.finken@gmail.com>
2016-09-10 21:48:46 -0500

    Complete 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,20 +3,49 @@
 # Release 1
 # Write a simple module
 
-module Shout
-end 
+# module Shout
+# end 
 
-require_relative "Shout" # Shout.singleton_methods
+# require_relative "Shout" 
 
-# create a module
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " :(" 
+# 	end
+# end
+
+
+
+
 module Shout
-	def self.yell_angrily(words)
+
+	def yell_angrily(words)
 		words + "!!!" + " :("
+		puts "#{words}. This is terrible!"
 	end
+
+	def yell_happily(words)
+		words + "HURRAH" + " :D" 
+		puts "I did it! #{words}"
+	end
+
 end
 
-module Shout
-	def self.yell_happily(words)
-		words + "!!!" + " :(" 
-	end
-end 
+
+class Child
+	include Shout
+end
+
+class Person
+	include Shout
+end
+
+child = Child.new 
+child.yell_happily("YAY") 
+
+person = Person.new
+person.yell_angrily("WTF")
+
