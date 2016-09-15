4e8dd182f1c2824c86a49a6f9fb0cff038b5e9fe
bexfinken <bex.finken@gmail.com>
2016-09-14 19:32:05 -0500

    Pseudocode

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+// Create two arrays: one colors and one names
+// Create an object: Colors are the horses' names, and the values are the colors 
+
+colors = ["periwinkle", "fuschia", "olive", "teal"]
+names = ["Paco", "Archibald", "Roger" "Xochilt"]
+
