7f539d841784ebe9141b76ab5047288e4cba68e0
bexfinken <bex.finken@gmail.com>
2016-09-16 16:36:44 -0500

    change

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,17 +1,114 @@
-class Calculator
-  def add(x,y)
-    x + y
-  end
+# #Question 3
 
-  def subtract(x,y)
-    x - y
-  end
+# class Calculator
+# initialize(add, subract, multiply, divide)
+# @add
+# @subract
+# @multiply
+# divide
 
-  def multiply(x,y)
-    x * y
-  end
+#   def self.add(x, y)
+#   x + y
+#   end
 
-  def divide (x,y)
-  	x / y
-  end
-end
+#   def self.subtract(x, y)
+#   x - y
+#   end
+
+#   def self.multiply(x, y)
+#   x * y
+#   end
+
+#   def self.divide(x, y)
+#   x / y
+#   end
+# end
+
+# #TEST
+# Calculator.add(15, 10)
+# Calculator.subtract(40, 15)
+# Calculator.multiply(5, 5)
+# Calculator.divide(100, 4)
+
+
+# #Question 4
+
+# class Calculator
+# initialize(add, subract, multiply, divide)
+# @add
+# @subract
+# @multiply
+# @divide
+
+# def calculator
+  
+#   puts "Welcome! What would you like to calculate? Enter symbol"
+#   symbol = gets.chomp
+  
+#   puts "Please enter the first number"
+#   number1 = gets.chomp.to_i
+  
+#   puts "Please enter the second number"
+#   number2 = gets.chomp.to_i
+  
+#   answer = case symbol
+#     when :+ then number1 + number2 
+#     when :- then number1 + number2
+#     when :* then number1 + number2
+#     when :/ then number1 + number2   
+#     else nil 
+#   end
+
+#   def self.add (number1, number2)
+#   number1 + number2
+#   end
+  
+#   def self.subtract (number1, number2)
+#   number1 - number2
+#   end
+  
+#   def self.multiply (number1, number2)
+#   number1 * number2
+#   end
+  
+#   def self.divide (number1, number2)
+#   number1 / number2
+#   end
+  
+#   puts "Your answer is: #{answer}! :D"
+# end
+
+
+
+
+# class Calculator
+# number1 = gets.chomp.to_i
+# number2 = gets.chomp.to_i
+
+# def keep_going(calculator)
+#   puts "Let's keep going!"
+#     list.each do |calculator |
+#       puts "#{answer}"
+#     end
+#   end
+
+#   def self.add (number1, number2)
+#   number1 + number2
+#   end
+  
+#   def self.subtract (number1, number2)
+#   number1 - number2
+#   end
+  
+#   def self.multiply (number1, number2)
+#   number1 * number2
+#   end
+  
+#   def self.divide (number1, number2)
+#   number1 / number2
+#   end
+  
+# end
+
+
+
