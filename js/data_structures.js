2fcb7bfaf5f1baa0bf953e01c888df52d876b722
bexfinken <bex.finken@gmail.com>
2016-09-16 16:38:53 -0500

    Release 0

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,19 @@
 // Create two arrays: one colors and one names
 // Create an object: Colors are the horses' names, and the values are the colors 
 
-var colors = ["periwinkle", "fuschia", "olive", "teal"]; // values
-var names = ["Paco", "Archibald", "Gertrude", "Xochilt"];   // keys
+var horse_color = ["periwinkle", "fuschia", "olive", "teal"]; // values
+var horse_name = ["Paco", "Archibald", "Gertrude", "Xochilt"];   // keys
 
+horse_color.push("pumpkin orange");
+horse_name.push("Robert")
 
+// Create an object using both arrays
+// keys = horse_name
+// values = horse_color. 
+// The code should work as long as both arrays are the same length 
 
-colors.push("pumpkin orange");
-names.push("Robert")
+horse = []
+
+for (var i = 0; i < colors.length; i++) {
+	console.log(colors[i]);
 
