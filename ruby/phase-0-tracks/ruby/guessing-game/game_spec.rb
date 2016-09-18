19e620d32134a5cd4f2e1f73c6e7968e2aaad2bb
Bex <bex.finken@gmial.com>
2016-09-17 22:57:11 -0500

    delete duplicates

--- ruby/phase-0-tracks/ruby/guessing-game/game_spec.rb
+++
@@ -1,6 +0,0 @@
-# User #1 input for magic word  magic_word = gets.chomp"
-# User #2 inputs their guesses for magic word guess = gets.chomp
-# Number of guesses = word.length
-# If User #2 guesses the same word more than once, that does not count against their guess limit (if/else)
-# User #2 gets to see the letters they get right as they input them (like hangman)
-# If they win, congratulate, if they lose, taunt them because you're a jerk (if/else)
