800f53244b0662888277b74d304882cacd29010d
Bex <bex.finken@gmial.com>
2016-09-19 06:12:03 -0500

    Create longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Write a function that takes an 
+// array of words or phrases and 
+// returns the longest word or phrase in the array. 
+
+function longest_phrase(phrase)
+	var = ["long phrase","longest phrase","longer phrase"]
+	console log(phrase.length)
