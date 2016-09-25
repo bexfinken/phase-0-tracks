228f9eb9506286f54e7b35b64e1bc59e3cfc30f7
Bex <bex.finken@gmial.com>
2016-09-25 16:57:00 -0500

    Complete 5.3 Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,15 +5,24 @@ spiders = [ "trap door", "wolf", "house", "brown recluse"]
 
 # Declare a hash
 pets = {
-	dog: "border collie"
-	fish: "beta"
+	dog: "border collie",
+	fish: "beta",
 	cat: "calico"
 }
 
 # Iterate through both using .each
 
 spiders.each do |spider|
-	puts "The #{spiders} is my least favorite spider."
+	puts "The #{spider} spider is my most favorite arthropod."
 end
 
-pets.each do ||
+pets.each do |animal, breed|
+	puts "I have the best #{animal} in the world; it's a #{breed}."
+end 
+
+
+# Iterate through the array using .map! 
+
+spiders.map! do |spider|
+	puts "OMG, is that a #{spider.upcase} SPIDER?!"
+end 
