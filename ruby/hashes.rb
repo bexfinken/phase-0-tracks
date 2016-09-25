b199265b4927d448af7831a34d2c261cc28c068f
Bex <bex.finken@gmial.com>
2016-09-25 12:01:13 -0500

    pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+# 5.2 Release 2: Use a Hash in a Program
+# 
+# This program should get
+# name, age, number of children, decor theme,
+# pets, budget, favorite color, phone number/email, preferred way to contact
+
+
+client = {
+	name:
+	age:
+	children:
+	decor_theme:
+	pets:
+	budget:
+	favorite_color:
+	phone:
+	email:
+	contact_preference:
+}
