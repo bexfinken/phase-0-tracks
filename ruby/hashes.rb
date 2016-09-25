26f9cb4c3ea9e717103cf4cbd28fcafb7c6f785c
Bex <bex.finken@gmial.com>
2016-09-25 14:46:19 -0500

    Complete 5.2 Release 2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,20 +1,81 @@
-# 5.2 Release 2: Use a Hash in a Program
-# 
-# This program should get
-# name, age, number of children, decor theme,
-# pets, budget, favorite color, phone number/email, preferred way to contact
+#5.2 Release 2: Use a Hash in a Program## This program should get# name, age, number of children, decor theme, #pets, budget, favorite color, phone number / email, preferred way to contact# create a hash to gather info# write code to prompt user for input
+# write code to allow users to update info# When done, the client should be able to exit
+
+puts "Welcome! Please enter the following information about your client. Type 'exit' when you're done."
+
+puts "Client's name?"
+name = gets.chomp
+
+puts "Client's age?"
+age = gets.chomp
+
+puts "Client's phone number?"
+phone = gets.chomp
+
+puts "Client's email?"
+email = gets.chomp
+
+puts "Would your client prefer to be contacted via email or phone?"
+contact = gets.chomp
+
+puts "Does your client have children?"
+children = gets.chomp
+	if children == "yes"
+	children = true
+	else children == "no"
+	children = false
+	end
+	
+puts "Does your client have pets?"
+pets = gets.chomp
+	if pets == "yes"
+	pets = true
+	else pets == "no"
+	pets = false
+	end
+
+if pets || children
+puts "Is the client interested in wear-resistant hardwood floors?"
+floors = gets.chomp
+end
+
+puts "What's your client's decor theme?"
+theme = gets.chomp
+
+puts "What's your client's favorite color?"
+color = gets.chomp
+
+puts "What's their budget?"
+budget = gets.chomp.to_i
 
 
 client = {
-	name:
-	age:
-	children:
-	decor_theme:
-	pets:
-	budget:
-	favorite_color:
-	phone:
-	email:
-	contact_preference:
+	name: "",
+	age: "",
+	phone: "",
+	email: "",
+	contact: "",
+	children: "",
+	pets: "",
+	floors: "",
+	theme: "",
+	color: "",
+	budget: ""
 }
+
+p client
+
+puts "Would you like to update any info?"
+update = gets.chomp
+if update == "yes"
+	puts "Please enter the category you'd like to change"
+	key = gets.to_sym
+	puts "Enter the new information"
+	input = gets.chomp
+	client [key] = [input]
+else 
+	puts "You're good to go!"
+end
+
+p client 
 
