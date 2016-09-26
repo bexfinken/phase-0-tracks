96cd0f3421571767d76ac550eadee191b6dbb627
Bex <bex.finken@gmial.com>
2016-09-26 04:23:36 -0500

    Pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# 5.5
+
+# Release 0: Attempt a Tricky Algorithm
+
+# Separate the first and last names
+# Split the first name into individual letters
+# Iterate through, use map! to change each vowel to next vowel; each consonants to next consonants
+# Split the last name into individual letters
+# Iterate same as first name
+# Print new names in swapped order
