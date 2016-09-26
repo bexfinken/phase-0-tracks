59e430f306ad9d5a1d6b7fb03fbd1a10a248f075
Bex <bex.finken@gmial.com>
2016-09-25 20:04:30 -0500

    Complete 5.3 Release 1 & 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,16 +1,17 @@
-# 5.3 Release 1: Use each, map, and map!
+# 5.3 
+# Release 1: Use each, map, and map!
 
-# Declare an array
+# # Declare an array
 spiders = [ "trap door spider", "wolf spider", "house spider", "brown recluse spider"]
 
-# Declare a hash
+# # Declare a hash
 pets = {
 	dog: "border collie",
 	fish: "beta",
 	cat: "calico"
 }
 
-# Iterate through both using .each
+# # Iterate through the array & has using .each
 
 spiders.each do |spider|
 	puts "The #{spider} is my most favorite arthropod."
@@ -21,8 +22,59 @@ pets.each do |animal, breed|
 end 
 
 
-# Iterate through the array using .map! 
+# # Iterate through the array using .map! 
 
 spiders.map! do |spider|
 	puts "OMG, is that a #{spider.upcase}?!"
 end 
+
+
+
+# Release 2: Use the Documentation
+
+array = [ 1, 2, 3, 4, 4, 3, "a", "b", "c", "a", "b"]
+
+pets = {
+	lizard: "Juan",
+	dog: "Joe",
+	cat: "Joe",
+	dragon: "Ignacio",
+	hamster: "Gertrude",
+	eaglet: "Sarah",
+	rock: "Ignacio",
+	poodle: "Maddie",
+}
+
+# # A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+
+array.delete_if { |x| x.odd?}
+p array
+
+pets.delete_if { |breed, name| name == "Joe"}
+p pets
+
+
+# # A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+
+array.keep_if { |x| x != "a"}
+p array
+
+pets.keep_if { |breed, name| name == "Maddie"}
+p pets
+
+
+# # A different method that filters a data structure for only items satisfying a certain condition 
+
+p array.count(4)
+
+pets.sort do |breed, name|
+	breed[1] <=> name[1]
+end
+
+
+
+
+
+
+
+
