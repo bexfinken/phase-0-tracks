fffdf2dbf8f1de0b1fb27033e7ad0d5fc4f22cbb
Bex <bex.finken@gmial.com>
2016-09-26 04:51:04 -0500

    split first and last names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,3 +8,14 @@
 # Split the last name into individual letters
 # Iterate same as first name
 # Print new names in swapped order
+
+vowels_array = ["a", "e", "i", "o", "u"]
+consonants_array = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+# Separate the first and last names
+def spy_name(real_name)
+  separate_names = real_name.split
+
+  first_name = separate_names[0].split("")
+  last_name = separate_names[1].split("")
+
