55cf6890a669d79b3d971be814d33490fd81b5d1
Bex <bex.finken@gmial.com>
2016-09-25 20:33:43 -0500

    first nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,5 +1,18 @@
 city_block = {
-	apartment_building: {}
+	apartment_building: {
+        address: '123 Street',
+        first_floor: {
+            one_bedroom: 4,
+            studio: 4
+        },
+        second_floor: {
+            one_bedroom: 2,
+            two_bedroom: 2
+        }
+	}
+
+
+	
 	single_family_home: {}
 	bodega:  {}
 	church: {}
