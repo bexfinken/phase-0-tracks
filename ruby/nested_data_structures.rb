63b8319409f0e7ed613adb0cbe278747d3f91c62
Bex <bex.finken@gmial.com>
2016-09-25 21:09:06 -0500

    Complete 5.4 Release 2

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,50 +3,50 @@
 city_block = {
 
 	apartment_building: {
-        address: '123 Street',
-        first_floor: {
-            one_bedroom: 4,
-            studio: 4
-        },
-        second_floor: {
-            one_bedroom: 2,
-            two_bedroom: 2
-        }
+    address: "123 Street",
+    first_floor: {
+	    one_bedroom: 4,
+	    studio: 4
+       },
+    second_floor: {
+     	one_bedroom: 2,
+      two_bedroom: 2
+       }
 	}
 
-
 	single_family_home: {
-		        address: '125 Street',
-        downstairs: [
-            "living room"
-            "bathroom"
-            "dining room"
-            "foyer"
+    address: "125 Street",
+    downstairs: [
+    	"living room"
+    	"bathroom"
+    	"dining room"
+    	"foyer"
         ],
-        upstairs: {
-            bedrooms: 3
-            bathrooms: 2
-            closets: 4
+      upstairs: {
+    		bedrooms: 3
+      	bathrooms: 2
+      	closets: 4
             
         }
-        attic: [
-            "storage"
-            "creepy nook"
+      attic: [
+    		"storage"
+    		"creepy nook"
         ]
 	}
 	bodega:  {
-		address: '127 Street',
-        retail_area: {
-            refrigerated: 3
-            groceries: 2
-            car_stuff: 2
-        },
-        back_area: [
-        	"desk"
-        	"safe"
-        ]
+		address: "127 Street",
+    retail_area: {
+			refrigerated: 3
+			groceries: 2
+			car_stuff: 2
+		},
+ 		back_area: [
+       "desk"
+       "safe"
+     ]
 	}
 	community_center: {
+		address: "129 Street",
 		gym: 1
 		basketball_court: 1
 		playroom: 2
