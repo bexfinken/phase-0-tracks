94a5b275490abf816c21489ec083e072569024eb
Bex <bex.finken@gmial.com>
2016-09-28 13:27:00 -0500

    Complete 6.3 Release 0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,9 +3,17 @@
 # Release 0
 
 class Santa
-	
+
+	def speak 
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!" 
+	end
+
 	def initialize
-		puts "Ho! Ho!"
+		puts "Initializing Santa instance ..."
 	end
 
 end
