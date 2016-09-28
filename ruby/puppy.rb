ed03284626b7ecfbb84e8becace2d1bbc100068e
Bex <bex.finken@gmial.com>
2016-09-28 13:10:32 -0500

    Complete 6.1

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,4 +1,17 @@
-class.Puppy
+# Puppy
+# Characteristics
+# 	cute
+# 	playful
+# 	barky
+# 	potty-trained
+
+# Behaviors
+# 	stiffs
+# 	wags tail
+# 	chews up everything
+# 	barks
+# 	cries
+# 	tinkles on the carpet
+
+class Puppy
 end
-
-
