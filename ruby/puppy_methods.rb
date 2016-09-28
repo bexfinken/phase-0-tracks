624a9c2b443e576fb69b623508debdfcf663b721
Bex <bex.finken@gmial.com>
2016-09-28 12:33:08 -0500

    Complete 6.2 Release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,34 +1,41 @@
 # 6.2 Mandatory Pairing: Instance Methods
 
-# Add driver code to the bottom to initialize Puppy instances
 
+# Release 1
+# Add driver code to the bottom to initialize Puppy instances
 class Puppy
 end
 
 class Puppy
 
-	def initialize
-  	puts "Initializing new puppy instance..."
-  end
-
-  def fetch(toy)
+	def fetch(toy)
     puts "I brought back the #{toy}!"
   end
 
-  def speak(times)
+# Add a speak method that takes an interger
+	def speak(times)
 		puts "Woof!" * times.to_i
   end
 
+# Add a roll over method that prints "**rolls over**"
   def roll_over
 		puts "**rolls over**"
 	end
 
+# Add a method that converts human years.to_i into dog years.to_i
   def dog_years(human_years)
 		puts  "if I was a dog, I'd be #{human_years.to_i * 7} in dog years."
 	end
 
-  def sit_pretty
+# Add my own trick
+ def sit_pretty
   	puts "I'm sitting pretty! *tips over* CLUNK!"
+  end
+
+ # Add a method "initialize" that prints "Initializing new puppy instance..."
+
+  def initialize
+  	puts "Initializing new puppy instance..."
   end
 
 end
