eec6a693df15391f6f2218aff0165387c9361f5d
Bex <bex.finken@gmial.com>
2016-09-30 13:01:29 -0500

    complete 5.3 release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,77 +1,7 @@
-# 6.3 Attributes
-
-# Releases 0 and 1
-
-# class Santa 
-
-# 	attr_accessor :gender
-# 	def initialize(gender, ethnicity)
-# 		puts "initializing Santa instance..."
-# 		@gender = gender
-# 		@ethnicity = ethnicity
-# 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Bltzen"] 
-   
-#  	end
-
-# 	def speak 
-# 		puts "Ho, ho, ho! Haaaappy holidays!"
-# 	end
-
-# 	def eat_milk_and_cookies(cookie)
-# 		puts "That was a good #{cookie}!" 
-# 	end
-
-# 	def age
-#   	@age = 0
-# 	end 
-
-# 	def celebrate_birthday!
-# 		@age += 1
-# 		puts "Happy birthday, Santa! You're #{@age} years old!"
-# 	end
-
-# 	def get_mad_at(reindeer)
-# 		@reindeer_ranking.delete(reindeer)
-#     @reindeer_ranking << reindeer
-# 		puts "Ooh...#{reinder} got moved to the back of the class!"
-# 		puts @reindeer_ranking
-# 	end
-
-# end
-
-# # I changed the array's name from  "santas" to "north_pole" because it was mixing me up having so many similarly named things.
-# north_pole = []
-
-# gender = ["fabulous", "Freddy Mercury", "gender neutral", "female", "bigender", "male", "female", "gender fluid", "cute", "fabulous"]
-# ethnicity = ["Chicano", "Nahuatl", "Inuit", "black", "Latinx", "white", "First Peoples", "Time Lord", "mixed", "Wookie"]
-
-
-# # I used .sample so the entire gender/ethinicity array wouldn't show up when I ran the code.
-# 10.times do |i|
-#   north_pole << Santa.new(gender[i], ethnicity[i])
-#   puts "Santa is a #{gender.sample} person of #{ethnicity.sample} heritage."
-#   puts get_mad_at("Rudolph")
-#  end
-
-
-
-
-
-# Release 2
-
-# Add three attribute-changing methods to your Santa class:
-		# 
-		# get_mad_at takes areindeer's name as an argument, and moves that reindeer in last place in the reindeer rankings.
-		# The @gender attribute should have a setter method that allows @gender to be reassigned from outside the class definition.
-		# add two "getter" methods as well:
-
-# The method age should simply return @age.
-# The method ethnicity should return @ethnicity.
-
 class Santa 
-	# attr_accessor :gender
-	# attr_accessor :ethnicity
-	# attr_accessor :age
+	attr_accessor :gender # this replaces getter methods
+	attr_accessor :ethnicity # this replaces getter methods
+	attr_accessor :age # this replaces getter methods
 	
 	def initialize(gender, ethnicity)
 		puts "initializing Santa instance..."
@@ -89,16 +19,8 @@ class Santa
 		puts "That was a good #{cookie}!" 
 	end
 
-	def age # getter method
-  	@age
-	end 
-
 	def age=(age) # setter method
 		@age = age
-	end
-
-	def ethnicity # getter method
-		@ethnicity
 	end
 
 	def ethnicity=(ethnicity)# setter method
@@ -117,11 +39,6 @@ class Santa
     puts "#{reindeer} just went to the back of the line!"
   end
 
-  # def celebrate_birthday(age) 
-  # 	age + 1
-  # 	puts age
-  # end
-
 end
 
 north_pole = [] # I changed the array name--too many things named santa
@@ -134,3 +51,6 @@ santa = Santa.new(gender, ethnicity)
 santa.get_mad_at("Prancer")
 santa.celebrate_birthday(100)
 	
+
+
+
