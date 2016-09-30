852cec9f395fd9e4ac3f2bcbadfdee3b9c22328e
Bex <bex.finken@gmial.com>
2016-09-30 02:59:27 -0500

    Complete Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,17 @@
 # 6.3 Attributes
 
-# Release 0
+# Releases 0 and 1
 
-class Santa
+class Santa 
+
+	def initialize(gender, ethnicity)
+		puts "initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Bltzen"] 
+   
+ 	end
+
 
 	def speak 
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -12,9 +21,37 @@ class Santa
 		puts "That was a good #{cookie}!" 
 	end
 
-	def initialize
-		puts "Initializing Santa instance ..."
-	end
+	def age
+  	@age = 0
+	end 
 
 end
+
+
+
+# I changed the array's name from  "santas" to "north_pole" because it was mixing me up having so many similarly named things.
+north_pole = []
+
+gender = ["fabulous", "Freddy Mercury", "gender neutral", "female", "bigender", "male", "female", "gender fluid", "cute", "fabulous"]
+ethnicity = ["Chicano", "Nahuatl", "Inuit", "black", "Latinx", "white", "First Peoples", "Time Lord", "All of the above", "Wookie"]
+
+
+# I used .sample so the entire gender/ethinicity array wouldn't show up when I ran the code.
+10.times do |i|
+  north_pole << Santa.new(gender[i], ethnicity[i])
+  puts "Santa is a #{gender.sample} person of #{ethnicity.sample} heritage."
+ end
+
+
+# Release 2
+
+
+
+
+
+
+
+
+
+
 
