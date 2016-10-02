35c41b0c7f01d0542d36ffed6bf4e5e2ee535d39
Bex <bex.finken@gmial.com>
2016-10-02 08:36:58 -0500

    complete release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,5 @@
 # Release 1 
 
-<<<<<<< HEAD
 # module Shout # This plus end are all you need to declare a module
 
 # 	def self.yell_angrily(words) # modules are defined on SELF
@@ -44,28 +43,3 @@ person.cheers("YAY")
 other_person = Person.new
 other_person.boo("You stink!")
 other_person.cheers("Woo hoo")
-=======
-# module Shout # Lines 3 and 4 are all you need to declare a module
-# end
-
-# require_relative 'shout' <--- In IRB, his loads the module, but it does't do anything
-# Shout.singleton_methods  <--- Because methods haven't been written, this returns empty [ ]
-
-module Shout
-  # def self.yell_angrily(words) # <--- methods of standalone modules are defined on the .self keyword
-  #   words + "!!!" + " :("
-  # end
-
-  def self.yelling_happily(cheers)  # self refers to the object defining the current scope.It changes when entering a different method or when defining a new module.
-
-
-  	cheers + "! YAY!"
-  end
-end
-
-
-
-# yell_angrily("Grrr!")
-yelling_happily("Hurrah")
->>>>>>> master
-
