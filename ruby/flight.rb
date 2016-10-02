b14cb592e1e92e8d870747774deb36716a320927
Bex <bex.finken@gmial.com>
2016-10-02 08:19:22 -0500

    complete 6.4 release 2

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -3,15 +3,20 @@
 
 module Flight
 	def take_off(altitude) # self kekyword isn't used for mixins
-		puts "Taking off and ascending until reaching #{altitude"}"
+		puts "Taking off and ascending until reaching #{altitude}"
 	end
 end
 
 class Bird
-	include Flight
+	include Flight # 'include' allows Bird class access to Flight module
 end
 
 class Plane
 	include Flight
 end
 
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
