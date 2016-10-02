f6b0434f7e97a6b556865748f66cc1c6df36b770
Bex <bex.finken@gmial.com>
2016-10-02 07:47:26 -0500

    add flight module to bird and plane classes

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,6 +1,17 @@
+# 6.4 Modules
+# Release 2
+
+module Flight
+	def take_off(altitude) # self kekyword isn't used for mixins
+		puts "Taking off and ascending until reaching #{altitude"}"
+	end
+end
+
 class Bird
+	include Flight
 end
 
 class Plane
+	include Flight
 end
 
