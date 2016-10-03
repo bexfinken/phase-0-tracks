7ebc1be050cfe680bfccb85fac85cb58e4ba3512
Bex <bex.finken@gmial.com>
2016-10-03 02:04:13 -0500

    got 0 failures

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -10,16 +10,15 @@ class TodoList
 		@list 
 	end
 
+	def add_item(item) 
+		@list.push(item)
+	end
 
-	# def add_item(item) 
-	# 	@list << item
-	# end
+	def delete_item(item)
+		@list.delete(item)		
+	end
 
-	# def delete_item(item)
-	# 	@list.delete(item)		
-	# end
-
-	# def get_item(index) 
-	# 	@list[index]
-	# end
+	def get_item(index) 
+		@list[index]
+	end
 end
