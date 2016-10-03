1c8784e901a60e31c0fee85da532a2ccd877bd92
Bex <bex.finken@gmial.com>
2016-10-03 03:33:55 -0500

    add it block for mocking reaction

--- ruby/6.6_game/game_spec.rb
+++ ruby/6.6_game/game_spec.rb
@@ -11,6 +11,7 @@ require_relative 'game'
 describe WordGame do 
 	let(:game) {WordGame.new ([kitten, "k", "i", "t", "e", "n"])}
 	
+	
 	it "gives you an attempt to guess per letter in the word" do
 		each_letter.one_turn("k", "i", "t", "e", "n")
 		expect(each_letter.one_turn). to eq ["k", "i", "t", "e", "n", 6]
@@ -27,9 +28,17 @@ describe WordGame do
 
 	it "populates a letter in each blank" do
 		game.correct_guess(puts " ")
-		expect(game.correct_guess).to eq "puts "correct_guess"
+		expect(game.correct_guess).to eq "["k", "i", "t", "e", "n"]"
+	end
 
+	it "cheers on a player win" do
+		game.win
+		expect(win.cheers).to eq "Hurrah"
+	end
 
-
+	it "boos on a player loss" do
+		game.lose
+		expect(loss.boo).to eq "WHOMP WHOMP"
+	end
 
 end
