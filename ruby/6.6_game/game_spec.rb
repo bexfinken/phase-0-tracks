6f7e0eb9de190c7de86b4561997e5c1307685f39
Bex <bex.finken@gmial.com>
2016-10-03 01:10:36 -0500

    pseudocode

---
+++ ruby/6.6_game/game_spec.rb
@@ -0,0 +1,7 @@
+# Player #1 should be prompted to enter a word/phrase 
+# Player #2 tries to guess what it is
+# Each letter in the word equals a turn at guessing
+# If a guess is repeated (e.g. guess 's' twice) it's considered one turn
+# When guessing correctly, the letters should populate the blank spots
+# If the player wins, they get cheered "YAY"
+# If the player loses, they get booed "WHOMP WHOMP"
