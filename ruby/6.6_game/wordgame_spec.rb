3fe888b62e6a711f8571051567ee1d600e93e451
Bex <bex.finken@gmial.com>
2016-10-04 14:58:53 -0500

    more in blocks

--- ruby/6.6_game/wordgame_spec.rb
+++ ruby/6.6_game/wordgame_spec.rb
@@ -5,15 +5,26 @@ describe Game do
 
 	it "prompts input" do
 		expect(game.input).to be_kind_of(String)
-
 	end
 
 	it "splits a word string into character strings" do
-		expect(game.splits).to eqto be_kind_of(Array)
+		expect(game.splits).to be_kind_of(Array)
+	end
 
+	it "sets the number of guesses"
+		expect(game.guesses).to eq(word.length?)
 	end
 
 	it "starts guess count at zero" do
 		expect(game.guess_count).to eq(0)
 	end
 
+	it "shows letter when guessed correctly"
+	
+	# SHOULD I MAYBE USE CONTEXT HERE?
+# context 'when correct guess' do
+  # it { is_expected.to respond_with letter }
+	# end
+# context 'when incorrect guess' do
+#   it { is_expected.to respond_with wah }
+# end
