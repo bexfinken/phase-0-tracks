7fca1c4f50fdcb9f73bb88dd73cb4db6731ab8bb
Bex <bex.finken@gmial.com>
2016-10-04 15:15:16 -0500

    specs ready to test

--- ruby/6.6_game/wordgame_spec.rb
+++ ruby/6.6_game/wordgame_spec.rb
@@ -19,12 +19,32 @@ describe Game do
 		expect(game.guess_count).to eq(0)
 	end
 
-	it "shows letter when guessed correctly"
-	
-	# SHOULD I MAYBE USE CONTEXT HERE?
+	it "shows letter when guessed correctly" do
+		expect(game.correct_guess).to be_kind_of(String)
+	end	
+# SHOULD I MAYBE USE CONTEXT HERE INSTEAD?
 # context 'when correct guess' do
-  # it { is_expected.to respond_with letter }
-	# end
-# context 'when incorrect guess' do
-#   it { is_expected.to respond_with wah }
+# 	it { is_expected.to respond_with letter }
 # end
+# context 'when incorrect guess' do
+# 	it { is_expected.to respond_with wah }
+# end
+	it "message for incorrect guesses" do
+		expect(game.wah).to be_kind_of(String)
+	end
+
+	it "doesn't count duplicate guesses" do
+		expect(game.duplicate).to eq( "I DON'T KNOW" )
+	end
+
+	it "happy message if sucessful" # ANOTHER GOOD PLACE FOR CONTEXT?
+		expect(game.hurrah).to eq "HURRAH! You did it!"
+	end
+
+	it "wah message if unsucessful"
+		expect(game.wah).to eq "Oh, sad! Maybe next time!"
+	end
+end
+end
+
+
