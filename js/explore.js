0dca4dd3c55c55abc412ec3b2eb33ee9aa5aad12
Bex <bex.finken@gmial.com>
2016-10-07 11:59:58 -0500

    7.1 Complete release 4

--- js/explore.js
+++ js/explore.js
@@ -3,9 +3,13 @@
 // Declare a function, set a parameter for string
 // Set loop counter to 0
 // Set condition to loop through the string until you run out of letters
-// Use an operator to add 1 to the counter every time the function loops through
+// Use an operator to subtract 1 to the counter every time the function loops through
 
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for (var i = 0; i <numbers.length; i+=2) {
-  console.log(numbers[i]);
+for (var i = -1; i <=numbers.length; i++) {
+  console.log(numbers[10-i]);
+}
+
+var str = ""
+for (var i = 0; i <)
