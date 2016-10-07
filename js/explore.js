b51ed715edf285832ba56e4784987dea9aec2a61
Bex <bex.finken@gmial.com>
2016-10-07 12:00:36 -0500

    7.1 Complete release 4

--- js/explore.js
+++ js/explore.js
@@ -6,10 +6,10 @@
 // Use an operator to subtract 1 to the counter every time the function loops through
 
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for (var i = -1; i <=numbers.length; i++) {
-  console.log(numbers[10-i]);
+function reverse(string) { 
+for (var i = 0; i <=string.length; i++) {
+  console.log(string[string.length-i]);
+  }
 }
 
-var str = ""
-for (var i = 0; i <)
+reverse("kitty")
