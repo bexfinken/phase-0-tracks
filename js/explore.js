370030ada5db3ed42d7102fab93aaecaf251cabf
Bex <bex.finken@gmial.com>
2016-10-07 11:27:57 -0500

    7.1 Release 4 Pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,12 +1,11 @@
-// Pseudocode a function that takes a string as a parameter and reverses the string. 
-// This isn't a task specific to JavaScript -- your pseudocode should be in plain English 
-// Remember that you can add strings in JavaScript, so '' + 'a' would result in the string 'a'. 
+// 7.1 Release 4
 
-// Under your commented pseudocode, implement your function in JavaScript. 
-// Add driver code that calls the function 
-// Add driver code that prints the variable if some condition is true. The condition can be a silly one, like 1 == 1.
+// Declare a function, set a parameter for string
+// Set loop counter to 0
+// Set condition to loop through the string until you run out of letters
+// Use an operator to add 1 to the counter every time the function loops through
 
 
-var str = "Loop over me!";
-for (var i = 0; i < str.length; i++) {
-  console.log(str[i]);
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (var i = 0; i <numbers.length; i+=2) {
+  console.log(numbers[i]);
