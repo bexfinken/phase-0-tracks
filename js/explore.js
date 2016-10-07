247bd55b0aed58e1ad705f127f8552141c014757
Bex <bex.finken@gmial.com>
2016-10-07 11:01:47 -0500

    7.1 Release 4 Pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,26 +1,12 @@
-// // 7.1 Release 4 Write a JavaScript Program
-// Create a function that takes a string as a parameter and reverses it.
-// variable 
-// Write driver code to execute the reversal
-// Store the result of the function in a variable
-// Add driver code to see if a condition is true
+// Pseudocode a function that takes a string as a parameter and reverses the string. 
+// This isn't a task specific to JavaScript -- your pseudocode should be in plain English 
+// Remember that you can add strings in JavaScript, so '' + 'a' would result in the string 'a'. 
 
-function reverseString(string) {
-  var myString = string; 
-  var newString = ""
-  for (var i = myString.length -1; 
-  	i >= 0;
-  	i--)
-  	newString += myString[i];
-  	
-  	return newString;
-}
-reverseString("!sdrawkcab ton ma I");
+// Under your commented pseudocode, implement your function in JavaScript. 
+// Add driver code that calls the function 
+// Add driver code that prints the variable if some condition is true. The condition can be a silly one, like 1 == 1.
 
 
-if ("tee" != "hee") {
-	var thing = reverseString("!sdrawkcab ton ma I");
-	console.log(thing);
-}
-
-
+var str = "Loop over me!";
+for (var i = 0; i < str.length; i++) {
+  console.log(str[i]);
