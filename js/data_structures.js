9ae3b5e4fb9d85e42812822957fda91998468370
Bex <bex.finken@gmial.com>
2016-10-07 21:16:24 -0500

    Complete 7.2 Release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,6 +13,7 @@
 // Keys(properties) = horse names values = colors
 // The objects should be any combinations of colors and horses as long as the arrays the same length
 
+// RELEASE 1: Build an Object
 var horseColors = ["Mildred", "Freddie", "Juan", "Guadalupe"];
 var horseNames = ["pink", "purple", "orange", "green"];
 
@@ -27,3 +28,28 @@ barn[horseNames[i]] = horseColors[i]
 }
 
 console.log(barn)
+
+
+
+// RELEASE 2: Build Many Objects Using a Constructor
+
+// constructor function
+function Car(make, model, isNew) {
+	
+	this.make = make;
+	this.model = model;
+	this.isNew = isNew;
+
+  this.rev = function() {console.log("VROOM."); }; 
+	console.log("Your chariot is ready!");
+}
+console.log("Dude, where's my car?");
+
+var myCar = new Car("Toyota", "Matrix", false);
+console.log(myCar)
+
+var yourCar = new Car("Dodge", "Dart", false);
+console.log(yourCar)
+
+var thatCarOnTheStreet = new Car("Volkswagon", "Beetle", true);
+console.log(thatCarOnTheStreet)
