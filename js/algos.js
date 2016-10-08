21b8d6f413813e4f0dbb45f3ce1c1b96166c504d
Bex <bex.finken@gmial.com>
2016-10-07 22:28:11 -0500

    Create array

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,10 @@
+
 // Release 0
 
-// Write a function that takes an 
-// array of words or phrases and 
-// // returns the longest word or phrase in the array. 
-// function longest_phrase(little, gargantuan) {
-//   return (little.length > gargantuan.length) ? little: gargantuan;
-// }
-// longest_phrase("little", "gargantuan");
+// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array. 
+
+var words = ["one", "two", "three", "four", "five"];
+
 
 
 // Release 1
