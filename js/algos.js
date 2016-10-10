0226a1bb7573c65c3ee590cb327b9238fad0da11
Bex <bex.finken@gmial.com>
2016-10-10 09:39:39 -0500

    pseudocode release 1

--- js/algos.js
+++ js/algos.js
@@ -20,9 +20,27 @@ console.log(largestWord); // print the longest word inside the container
 // I am not sure if it's still a function without the word "function" in there. I tried to fit it in. 
 
 // Release 1
+// Write a FUNCTION that takes two objects and checks if objects share at least one key-value pair. 
 
-// Write a function that takes two objects and checks to see if the objects share at least one key-value pair. (You'll keep adding to this file, so we suggest grouping functions at the top and testing at the bottom of the file.) If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, the function would return true, because at least one key-value pair matches between the two objects. If no pairs match, the function should return false. For this project, a string or identifier with the same name can be considered equivalent.
-// Add some driver code that tests both outcomes of your function.
+indexOf() which takes an object as its input and returns the index of that object 	the array, or -1 if the object is not in the array
+
+// Acessor method returns the index of an object of the array, or -1 if it's NOT in in the array 
+// 
+// IF at least one key-value pair matches between the two, return TRUE
+// 		If the accessor returns anything other than -1, the match is TRUE
+// IF no pairs match, the function should return FALSE. 
+// 		So, set the acessor method to return FALSE if -1 is the return
+// String or identifier with the same name can be considered equivalent.
+// ONLY for this project
+
+
+
+
+
+
+
+
+
 
 
 
