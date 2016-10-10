8dd62bf60d8383e25d2c851447141f1c82754541
Bex <bex.finken@gmial.com>
2016-10-10 09:51:29 -0500

    release 0 complete

--- js/algos.js
+++ js/algos.js
@@ -27,7 +27,7 @@ indexOf() which takes an object as its input and returns the index of that objec
 // Acessor method returns the index of an object of the array, or -1 if it's NOT in in the array 
 // 
 // IF at least one key-value pair matches between the two, return TRUE
-// 		If the accessor returns anything other than -1, the match is TRUE
+// 		IF the accessor returns anything other than -1, the match is TRUE
 // IF no pairs match, the function should return FALSE. 
 // 		So, set the acessor method to return FALSE if -1 is the return
 // String or identifier with the same name can be considered equivalent.
@@ -35,11 +35,16 @@ indexOf() which takes an object as its input and returns the index of that objec
 
 
 
-
-
-
-
-
+var words = ["one", "two", "three", "four", "five"];
+var largest = 0; //where you start the count 
+var largestWord = ""; // empty container
+for (var i = 0; i < words.length; i++) { // iterate
+    if (words[i].length > largest) { 
+        largest = words[i].length; // counts a word each time it loops
+        largestWord = words[i]; // shows the index of the longest word
+    }
+}
+console.log(largestWord); // print the longest word inside the container
 
 
 
