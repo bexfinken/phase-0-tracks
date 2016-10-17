00d39edaef8eb094539096bbd65d3c0351a643ca
Bex <bex.finken@gmial.com>
2016-10-17 11:02:40 -0500

    remove duplicates

--- databases/week8_project/week8_project.rb
+++
@@ -1,6 +0,0 @@
-# Write a Ruby program that uses persistent data to make your life better, or makes someone else's life better. Find several opportunities to try something not explicitly shown in the video. Can users create data? Retrieve it? Manipulate it? It's up to you. Push yourself, and decide with integrity when you are finished.
-
-
-require 'sqlite3'
-require 'faker'
-
