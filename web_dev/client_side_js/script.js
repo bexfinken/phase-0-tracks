b728dde85d0d99d99204a7ab0fee519f173cf9e9
Bex <bex.finken@gmial.com>
2016-10-20 21:41:37 -0500

    complete 9.2 release 1

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,8 +1,10 @@
 console.log("Is the mic on?");
 
+// This starts out the image hidden from view
 var img = document.getElementById("godzilla"); 
 img.style.visibility = "hidden";
 
+// This is the event that makes the image appear
 function popUpGodzilla() {
 	var img = document.getElementById("godzilla"); 
 	img.style.visibility = "visible";
