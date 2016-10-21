755e34ca395c108ef8de093851f02e2dc6ddc405
Bex <bex.finken@gmial.com>
2016-10-20 21:34:02 -0500

    event works. YAY

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,12 @@
 console.log("Is the mic on?");
+
+var img = document.getElementById("godzilla"); 
+img.style.visibility = "hidden";
+
+function popUpGodzilla() {
+	var img = document.getElementById("godzilla"); 
+	img.style.visibility = "visible";
+}
+
+var button = document.getElementById("button");
+button.addEventListener("click", popUpGodzilla);
